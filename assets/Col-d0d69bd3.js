2044d742492e2af2f20499d3fdc929b70c020f75
DannyGalvan <cgalvan29111999@gmail.com>
2023-10-28 00:01:10 -0600

    Updates

--- assets/Col-d0d69bd3.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-bc3ac4c7.js";const i=({children:t,xs:o,sm:s,md:c,lg:l,className:r})=>{const a=[o?`col-${o}`:"col",s?`col-sm-${s}`:"",c?`col-md-${c}`:"",l?`col-lg-${l}`:"",r,"px-2"];return e.jsx("div",{className:a.join(" "),children:t})};export{i as C};
